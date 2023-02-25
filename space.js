9016f6db31528bf04dba15b85229aa985a493ba3
cybertexts <125028977+cybertexts@users.noreply.github.com>
2023-02-25 19:30:49 +0200

    Create space.js

---
+++ space.js
@@ -0,0 +1,50 @@
+particlesJS("space", {
+  "particles": {
+    "number": {
+      "value": 50,
+      "density": {
+        "enable": true,
+        "value_area": 500
+      }
+    },
+    "color": {
+      "value": "#fff"
+    },
+    "opacity": {
+      "value": 1,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0.4,
+        "sync": false
+      }
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "size": {
+      "value": 2,
+      "random": true
+    },
+    "line_linked": {
+      "enable": true
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "right",
+      "straight": false
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true
+      },
+      "onclick": {
+        "enable": true
+      }
+    }
+  }
+});
